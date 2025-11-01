479c5b5b0329080b4ca21eddfb02c92d6961303a
김현빈 <132333588+hyunbindev@users.noreply.github.com>
2025-11-01 13:50:14 +0900

    Add files via upload

---
+++ graffiti/src/main/java/com/hyunbindev/graffiti/GraffitiApplication.java
@@ -0,0 +1,13 @@
+package com.hyunbindev.graffiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraffitiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraffitiApplication.class, args);
+	}
+
+}
