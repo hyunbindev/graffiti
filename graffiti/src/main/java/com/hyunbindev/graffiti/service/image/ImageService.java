1fe27928be83f98e21232d775cb1eee5cf0cd3d7
brutalKim <132333588+brutalKim@users.noreply.github.com>
2025-11-27 17:48:57 +0900

    image url rewrite

--- graffiti/src/main/java/com/hyunbindev/graffiti/service/image/ImageService.java
+++ graffiti/src/main/java/com/hyunbindev/graffiti/service/image/ImageService.java
@@ -2,12 +2,13 @@ package com.hyunbindev.graffiti.service.image;
 
 import java.io.BufferedInputStream;
 import java.io.InputStream;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -29,6 +30,8 @@ import lombok.extern.slf4j.Slf4j;
 public class ImageService {
 	private final MinioClient minioClient;
 	private final RabbitTemplate rabbitTemplate;
+	@Value("${client.baseUrl}")
+	String baseUrl;
 	final int BUFFER_SIZE = 8192;
 	/**
 	 * 이미지 저장 로직
@@ -63,14 +66,16 @@ public class ImageService {
 	 */
 	public String getPresignedUrl(String objectName) {
 		try {
-		    return minioClient.getPresignedObjectUrl(
+		    String presignedUrl=minioClient.getPresignedObjectUrl(
 		        GetPresignedObjectUrlArgs.builder()
 		            .method(Method.GET)
 		            .bucket(MinioConfig.BUCKET_NAME)
 		            .object(objectName)
 		            .expiry(60*60)
-		            .build()
-		    );
+		            .build());
+		    URI uri = new URI(presignedUrl);
+
+		    return baseUrl + "/media" +uri.getPath();
 		} catch (Exception e) {
 		    e.printStackTrace();
 		    throw new CommonAPIException("이미지 조회 실패",HttpStatus.INTERNAL_SERVER_ERROR);
