b6aa6a56ac49472b49631242cf76e4fa29e4e4a0
brutalKim <132333588+brutalKim@users.noreply.github.com>
2025-11-27 15:03:18 +0900

    config redis host port

--- graffiti/src/main/java/com/hyunbindev/graffiti/config/RedisConfig.java
+++ graffiti/src/main/java/com/hyunbindev/graffiti/config/RedisConfig.java
@@ -1,21 +1,25 @@
 package com.hyunbindev.graffiti.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
-import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableRedisRepositories(basePackages = "com.hyunbindev.graffiti.repository.redis")
 public class RedisConfig {
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
 	@Bean
 	LettuceConnectionFactory connectionFactory() {
-		return new LettuceConnectionFactory();
+		return new LettuceConnectionFactory(host,port);
 	}
 	@Bean("redisViewCountTemplate")
 	public RedisTemplate<String, Object> redisViewCountTemplate(LettuceConnectionFactory connectionFactory){
